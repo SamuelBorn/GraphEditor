700764680b3a81ef9771f6e6723df341c22a75d7
Samuel Born <samulhessen@gmail.com>
2021-01-11 20:10:57 +0100

    antoher problem

--- GraphEditV2/src/main/java/graphEdit/MainGUI.java
+++ GraphEditV2/src/main/java/graphEdit/MainGUI.java
@@ -46,7 +46,6 @@ public class MainGUI implements Runnable {
         frame.setLocationByPlatform(true);
 
 
-
         frame.setVisible(true);
     }
 
@@ -84,8 +83,48 @@ public class MainGUI implements Runnable {
         editOptionGroup.add(setStart);
 
         loadTestGraph.addActionListener(e -> {
+            if (buttonVertexBiMap.values().size() > 0) {
+                JOptionPane.showMessageDialog(frame, "Loading a test graph is only allowed as the first action.");
+                return;
+            }
+            buttonPlacer.placeButton(100, 100);
+            buttonPlacer.placeButton(200, 100);
+            buttonPlacer.placeButton(200, 200);
+            buttonPlacer.placeButton(300, 100);
+            buttonPlacer.placeButton(400, 100);
+            buttonPlacer.placeButton(500, 200);
+            buttonPlacer.placeButton(300, 200);
+            buttonPlacer.placeButton(500, 100);
 
+            Vertex[] testVertices = new Vertex[10];
+            int i = 0;
+            for (Vertex value : buttonVertexBiMap.values()) {
+                testVertices[i] = value;
+                i++;
+            }
 
+            new SetStartVertexStrategy(this).editGGraph(buttonVertexBiMap.inverse().get(testVertices[0]));
+            new SetFinalStrategy(this).editGGraph(buttonVertexBiMap.inverse().get(testVertices[6]));
+            new SetFinalStrategy(this).editGGraph(buttonVertexBiMap.inverse().get(testVertices[7]));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[0], testVertices[1], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[0], testVertices[2], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[1], testVertices[2], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[1], testVertices[3], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[2], testVertices[2], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[2], testVertices[2], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[3], testVertices[6], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[3], testVertices[4], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[4], testVertices[4], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[4], testVertices[7], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[5], testVertices[5], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[5], testVertices[4], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[6], testVertices[4], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[6], testVertices[5], '/'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[7], testVertices[4], '*'));
+            new AddEdgeStrategy(this).placeEdge(new Edge(testVertices[7], testVertices[5], '/'));
+
+            contentPane.revalidate();
+            contentPane.repaint();
         });
 
         minimize.addActionListener(e -> {
